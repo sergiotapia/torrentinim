929f09c9ddbc504502a88755760a54175ca4cce0
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2021-05-26 00:13:06 -0400

    Fix NUKE_MY_DATABASE flag not functioning properly

--- src/database.nim
+++ src/database.nim
@@ -13,6 +13,7 @@ proc initDatabase*(): string =
   let db = open("torrentinim-data.db", "", "", "")
 
   db.exec(sql"DROP TABLE IF EXISTS torrents")
+  db.exec(sql"DROP TABLE IF EXISTS torrents_index")
   db.exec(sql"PRAGMA case_sensitive_like = true;")
   echo "[database] Initializing database"
   db.exec(sql"""CREATE TABLE IF NOT EXISTS torrents (
@@ -63,40 +64,3 @@ proc initDatabase*(): string =
   """)
 
   db.close()
-
-# proc latest*(limit: int): seq[Torrent] =
-#   let db = open("torrentinim-data.db", "", "", "")
-#   let torrents = db.getAllRows(sql"SELECT name, uploaded_at, canonical_url, magnet_url, size, seeders, leechers FROM torrents LIMIT ?", limit)
-#   for row in torrents:
-#     result.add(
-#       Torrent(
-#         name: row[0],
-#         uploaded_at: parse(row[1], "yyyy-MM-dd'T'HH:mm:sszzz"),
-#         canonical_url: row[2],
-#         magnet_url: row[3],
-#         size: row[4],
-#         seeders: parseInt(row[5]),
-#         leechers: parseInt(row[6]),
-#       )
-#     )
-#   db.close()
-
-# proc search*(query: string, page: int): seq[Torrent] =
-#   let perPage = 5
-#   let skip = if page == 1: 0 else: page * perPage
-#   let db = open("torrentinim-data.db", "", "", "")
-#   let torrents = db.getAllRows(sql"SELECT name, uploaded_at, canonical_url, magnet_url, size, seeders, leechers FROM torrents WHERE name LIKE '%' || ? || '%' LIMIT ?, ?", query, skip, perPage)
-
-#   for row in torrents:
-#     result.add(
-#       Torrent(
-#         name: row[0],
-#         uploaded_at: parse(row[1], "yyyy-MM-dd'T'HH:mm:sszzz"),
-#         canonical_url: row[2],
-#         magnet_url: row[3],
-#         size: row[4],
-#         seeders: parseInt(row[5]),
-#         leechers: parseInt(row[6]),
-#       )
-#     )
-#   db.close()
